907eb89cf57b50cb572f2d0be37325017abbed86
MansoorMusthafa14 <mansooryasmin14@gmail.com>
2022-06-01 19:19:04 +0530

    helo

---
+++ src/routes/logger.js
@@ -0,0 +1,7 @@
+const endpoint="www.functionup.org"
+
+const file= function(){
+    console.log("i am mansoor")
+}
+
+module.exports.endpoint=endpoint
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
 const router = express.Router();
-
-router.get('/test-me', function (req, res) {
+const dhoni=require('./logger')
+router.get('/test-me1', function (req, res) {
+    console.log("my first project"+dhoni.endpoint)
     res.send('My first ever api!')
 });
 
+router.get('/test-me2', function (req, res) {
+    res.send('My second ever api!')
+});
+router.get('/test-me3', function (req, res) {
+    res.send('My third ever api!')
+});
 module.exports = router;
 // adding this comment for no reason
