cc0df04c7cabdd415883cb2a043e245f2db9a3f1
MansoorMusthafa14 <mansooryasmin14@gmail.com>
2022-06-15 15:08:59 +0530

    assignemnt middele ware 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://mansoor17:Mansoor14@cluster0.dxridun.mongodb.net/13-June-22", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
